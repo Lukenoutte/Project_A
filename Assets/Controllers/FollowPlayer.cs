219c13eb92023dab2280f07fca04aa213cc759e9
Lucas Lima <lucas.alima@souunit.com.br>
2020-04-27 01:05:04 -0300

    fix dog

--- Assets/Controllers/FollowPlayer.cs
+++ Assets/Controllers/FollowPlayer.cs
@@ -5,75 +5,25 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
 
+    public float speed;
     public GameObject player;
-    private Rigidbody2D rbPlayer;
-    public float followDistance;
-    public float distCheck;
-    private SpriteRenderer sRenderer;
-
-    private Rigidbody2D rb;
+    private Transform target;
+    public int stoppingDistance;
 
 
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        sRenderer = GetComponent<SpriteRenderer>();
-        rbPlayer = GameObject.Find("Player").GetComponent<Rigidbody2D>();
-        if (!rbPlayer)
-        {
-            Debug.Log("The player gameobject was not set");
-        }
-
-       
-
-        if (!sRenderer)
-        {
-            Debug.Log("sRenderer was not set");
-        }
-        else
-        {
-            sRenderer.enabled = false;
-        }
-
-        if (followDistance == 0)
-        {
-            Debug.Log("Please set distance higher then 0");
-        }
-    }
 
     void Start()
     {
-        StartCoroutine(Viewable()); //enable the image
-    }
+        target = player.GetComponent<Transform>();
 
-    void Update()
-    {
-        float dist = Vector2.Distance(rb.position, rbPlayer.position); //distance between player and follower
-
-        if (dist < distCheck)
-        {
-            //follower is to close to the player
-            //Debug.Log(dist);
-            StopAllCoroutines(); //stop future movements
-            rb.velocity = Vector2.zero; //stop in place
-        }
-        else
-        {
-            StartCoroutine(ReplaceRigidBody(rbPlayer.position)); //move closer to player
-        }
 
     }
 
-    IEnumerator ReplaceRigidBody(Vector2 setTo)
+    void FixedUpdate()
     {
-        yield return new WaitForSeconds(followDistance);
-        sRenderer.enabled = true;
-        rb.position = setTo;
-    }
+       
+        if (Mathf.Round(Vector2.Distance(transform.position, target.position)) > stoppingDistance)
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
 
-    IEnumerator Viewable()
-    {
-        yield return new WaitForSeconds(followDistance);
-        sRenderer.enabled = true;
     }
 }
