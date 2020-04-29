b8d9afd9e6e173b8eeb5c95e52f115b1615312dd
Lucas Lima <lucas.alima@souunit.com.br>
2020-04-29 11:32:21 -0300

    colider dog

---
+++ Assets/Controllers/DogController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogController : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject player;
+    private Transform target;
+    public int stoppingDistance;
+
+
+
+
+    void Start()
+    {
+        target = player.GetComponent<Transform>();
+
+        
+    }
+
+    void FixedUpdate()
+    {
+       
+        if (Mathf.Round(Vector2.Distance(transform.position, target.position)) > stoppingDistance)
+        {
+            
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+       
+    }
+
+
+}
